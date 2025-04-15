8035de9b644abdb762839dc008be785683dcebf4
heliang_wsl <brighthe98@gmail.com>
2025-04-15 17:36:46 +0800

    update

--- soptx/tests/test_cantilever_2d_wsl.py
+++ soptx/tests/test_cantilever_2d_wsl.py
@@ -1,4 +1,4 @@
-"""测试不同后端、优化器、滤波器、网格下的 3D 悬臂梁"""
+"""测试不同后端、优化器、滤波器、网格下的 2D 悬臂梁"""
 
 from dataclasses import dataclass
 from typing import Literal, Optional, Union, Dict, Any
@@ -7,29 +7,26 @@ from pathlib import Path
 from fealpy.backend import backend_manager as bm
 from fealpy.typing import TensorLike
 from fealpy.decorator import cartesian
-from fealpy.mesh import UniformMesh3d, TetrahedronMesh
+from fealpy.mesh import UniformMesh2d, TriangleMesh
 from fealpy.functionspace import LagrangeFESpace, TensorFunctionSpace
 
 from soptx.material import (
                             DensityBasedMaterialConfig,
                             DensityBasedMaterialInstance,
                         )
-from soptx.pde import Cantilever3dData1
+from soptx.pde import Cantilever2dData1, Cantilever2dData2
 from soptx.solver import (ElasticFEMSolver, AssemblyMethod)
 from soptx.filter import (SensitivityBasicFilter, 
                           DensityBasicFilter, 
                           HeavisideProjectionBasicFilter)
-from soptx.opt import (ComplianceObjective, ComplianceConfig,
-                       VolumeConstraint, VolumeConfig)
-
+from soptx.opt import ComplianceObjective, VolumeConstraint
 from soptx.opt import OCOptimizer, MMAOptimizer, save_optimization_history, plot_optimization_history
-
 @dataclass
 class TestConfig:
     """Configuration for topology optimization test cases."""
     backend: Literal['numpy', 'pytorch', 'jax']
     device: Literal['cpu', 'cuda']
-    pde_type: Literal['cantilever_3d_1']
+    pde_type: Literal['cantilever_2d_1', 'cantilever_2d_2']
 
     elastic_modulus: float
     poisson_ratio: float
@@ -37,20 +34,17 @@ class TestConfig:
 
     domain_length : float
     domain_width : float
-    domain_height : float
 
     load : float
 
     volume_fraction: float
     penalty_factor: float
 
-    mesh_type: Literal['uniform_mesh_3d', 'tetrahedron_mesh']
+    mesh_type: Literal['uniform_mesh_2d', 'triangle_mesh']
     nx: int
     ny: int
-    nz: int
     hx: float
     hy: float
-    hz: float
 
     p: int
     
@@ -78,40 +72,43 @@ def create_base_components(config: TestConfig):
     elif config.backend == 'jax':
         bm.set_backend('jax')
 
-    if config.pde_type == 'cantilever_3d_1':
-        pde = Cantilever3dData1(
+    if config.pde_type == 'cantilever_2d_2':
+        pde = Cantilever2dData2(
                     xmin=0, xmax=config.domain_length,
                     ymin=0, ymax=config.domain_width,
-                    zmin=0, zmax=config.domain_height,
                     T = config.load
                 )
-        if config.mesh_type == 'uniform_mesh_3d':
-            extent = [0, config.nx, 0, config.ny, 0, config.nz]
-            origin = [0.0, 0.0, 0.0]
-            mesh = UniformMesh3d(
-                        extent=extent, h=[config.hx, config.hy, config.hz], origin=origin,
-                        ipoints_ordering='zyx',
-                        device=config.device
+        if config.mesh_type == 'triangle_mesh':
+            mesh = TriangleMesh.from_box(box=pde.domain(), 
+                                        nx=config.nx, ny=config.ny)
+    elif config.pde_type == 'cantilever_2d_1':
+        pde = Cantilever2dData1(
+                    xmin=0, xmax=config.domain_length,
+                    ymin=0, ymax=config.domain_width,
+                    T = config.load
+                )
+        if config.mesh_type == 'uniform_mesh_2d':
+            extent = [0, config.nx, 0, config.ny]
+            origin = [0.0, 0.0]
+            mesh = UniformMesh2d(
+                        extent=extent, h=[config.hx, config.hy], origin=origin,
+                        ipoints_ordering='yx',
+                        device=config.device,
                     )
-        elif config.mesh_type == 'tetrahedron_mesh':
-            mesh = TetrahedronMesh.from_box(box=pde.domain(), 
-                                            nx=config.nx, ny=config.ny, nz=config.nz)
 
     GD = mesh.geo_dimension()
     
-    p = 1
+    p = config.p
     space_C = LagrangeFESpace(mesh=mesh, p=p, ctype='C')
     tensor_space_C = TensorFunctionSpace(space_C, (-1, GD))
-    print(f"CGDOF: {tensor_space_C.number_of_global_dofs()}")
     space_D = LagrangeFESpace(mesh=mesh, p=p-1, ctype='D')
-    print(f"DGDOF: {space_D.number_of_global_dofs()}")
     
     material_config = DensityBasedMaterialConfig(
                             elastic_modulus=config.elastic_modulus,            
                             minimal_modulus=config.minimal_modulus,         
                             poisson_ratio=config.poisson_ratio,            
-                            plane_assumption="3D",
-                            device=config.device,    
+                            plane_assumption="plane_stress", 
+                            device=config.device,      
                             interpolation_model="SIMP",    
                             penalty_factor=config.penalty_factor
                         )
@@ -131,22 +128,21 @@ def create_base_components(config: TestConfig):
     kwargs = bm.context(node)
     @cartesian
     def density_func(x: TensorLike):
-        val = config.volume_fraction * bm.ones(x.shape[0], **kwargs)
-        # val = bm.ones(x.shape[0], **kwargs)
+        # val = config.volume_fraction * bm.ones(x.shape[0], **kwargs)
+        val = bm.ones(x.shape[0], **kwargs)
         return val
     rho = space_D.interpolate(u=density_func)
 
-    obj_config = ComplianceConfig(diff_mode=config.diff_mode)
-    objective = ComplianceObjective(solver=solver, config=obj_config)
-    cons_config = VolumeConfig(diff_mode=config.diff_mode)
+    objective = ComplianceObjective(solver=solver)
     constraint = VolumeConstraint(solver=solver, 
-                                volume_fraction=config.volume_fraction,
-                                config=cons_config)
+                                volume_fraction=config.volume_fraction)
     
     return pde, rho, objective, constraint
 
 def run_basic_filter_test(config: TestConfig) -> Dict[str, Any]:
-    """测试 filter 类不同滤波器的正确性."""
+    """
+    测试 filter 类不同滤波器的正确性.
+    """
     pde, rho, objective, constraint = create_base_components(config)
     mesh = objective.solver.tensor_space.mesh
 
@@ -157,7 +153,7 @@ def run_basic_filter_test(config: TestConfig) -> Dict[str, Any]:
     elif config.filter_type == 'density':
         filter = DensityBasicFilter(mesh=mesh, rmin=config.filter_radius, domain=pde.domain())
     elif config.filter_type == 'heaviside':
-        filter = HeavisideProjectionBasicFilter(mesh=mesh, rmin=config.filter_radius, domain=pde.domain())   
+        filter = HeavisideProjectionBasicFilter(mesh=mesh, rmin=config.filter_radius, domain=pde.domain())  
 
     if config.optimizer_type == 'oc':
         optimizer = OCOptimizer(
@@ -215,49 +211,34 @@ def run_basic_filter_test(config: TestConfig) -> Dict[str, Any]:
         'mesh': mesh
     }
 
+
 if __name__ == "__main__":
     base_dir = '/home/heliang/soptx/soptx/vtu'
-    '''
-    参数来源论文: An efficient 3D topology optimization code written in Matlab
-    '''
-    # backend = 'numpy'
-    backend = 'pytorch'
-    # backend = 'jax'
-    # device = 'cpu'
-    device = 'cuda'
-    pde_type = 'cantilever_3d_1'
-    # mesh_type = 'tetrahedron_mesh'
-    mesh_type = 'uniform_mesh_3d'
+    # 参数来源论文: Efficient topology optimization in MATLAB using 88 lines of code
+    backend = 'numpy'
+    device = 'cpu'
+    pde_type = 'cantilever_2d_1'
     optimizer_type = 'oc'
     filter_type = 'sensitivity'
-    # nx, ny, nz = 60, 20, 4
-    # filter_radius = 1.5
-    nx, ny, nz = 120, 40, 8
-    filter_radius = 3.0
-    config_basic_filter = TestConfig(
-        backend=backend,
-        device=device,
-        pde_type=pde_type,
-        elastic_modulus=1, poisson_ratio=0.3, minimal_modulus=1e-9,
-        domain_length=nx, domain_width=ny, domain_height=nz,
-        load=-1,
-        volume_fraction=0.3,
-        penalty_factor=3.0,
-        mesh_type=mesh_type, nx=nx, ny=ny, nz=nz, hx=1, hy=1, hz=1,
-        p = 1,
-        assembly_method=AssemblyMethod.FAST,
-        # assembly_method=AssemblyMethod.STANDARD,
-        # assembly_method=AssemblyMethod.SYMBOLIC,
-        # solver_type='direct', solver_params={'solver_type': 'mumps'},
-        # solver_type='direct', solver_params={'solver_type': 'cupy'},
-        # solver_type='direct', solver_params={'solver_type': 'scipy'},
-        solver_type='cg', solver_params={'maxiter': 5000, 'atol': 1e-12, 'rtol': 1e-12},
-        diff_mode='manual',
-        # diff_mode='auto',
-        optimizer_type=optimizer_type, max_iterations=180, tolerance=0.01,
-        filter_type=filter_type, filter_radius=filter_radius,
-        save_dir=f'{base_dir}/{device}_{backend}_{pde_type}_{mesh_type}_{optimizer_type}_{filter_type}_{nx*ny*nz}',
-    )
-
-    result = run_basic_filter_test(config_basic_filter )
+    nx, ny = 160, 100
+    filter_radius = 6.0
+    config_sens_filter = TestConfig(
+            backend=backend,
+            device=device,
+            pde_type=pde_type,
+            elastic_modulus=1, poisson_ratio=0.3, minimal_modulus=1e-9,
+            domain_length=nx, domain_width=ny,
+            load=-1,
+            volume_fraction=0.4,
+            penalty_factor=3.0,
+            mesh_type='uniform_mesh_2d', nx=nx, ny=ny, hx=1, hy=1,
+            p = 1,
+            assembly_method=AssemblyMethod.FAST,
+            solver_type='direct', solver_params={'solver_type': 'mumps'},
+            diff_mode='manual',
+            optimizer_type=optimizer_type, max_iterations=200, tolerance=0.01,
+            filter_type=filter_type, filter_radius=filter_radius,
+            save_dir=f'{base_dir}/{backend}_{pde_type}_{optimizer_type}_{filter_type}_{nx*ny}',
+        )
+    result1 = run_basic_filter_test(config_sens_filter)
     
